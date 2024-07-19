8782035f80c8a1fb5f4eb832c21cf7b0cc1a66bd
yhamerith <gabriel.santos.marcar1@gmail.com>
2024-07-18 21:57:13 -0300

    Startapp galeria;
    Criação de urls;
    
    Configuração na urls do setup para 'puxar' a urls da pasta galeria;
    
    Criação de index na views;
    
    Criação de Templates e index.html;
    
    Texto base adicionado na página inicial;

--- setup/settings.py
+++ setup/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'galeria',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
