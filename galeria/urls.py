fb56061dc0942f86f942536b3fc0675ffe07dd15
yhamerith <gabriel.santos.marcar1@gmail.com>
2024-07-19 22:20:01 -0300

    Criação de templates;
    Ajustado template na config.py;
    
    Aplicação de HTML feito pela Alura, editando os códigos para utilizar o Static e aplicar o arquivo de CSS;
    
    Ajustes do Static para puxar as imagens corretamente;
    
    Adicionando outra página e, em desafio, colocar o Statics e fazer com que a página seja devidamente chamada ao clicar no index.html;
    
    Para tal processo, mudei o diretório no index, assim o template identifica a página certa o urls.py identificar e puxar corretamente o view.py;

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index),
+    path('', views.index),
+    path('galeria/', views.imagem),
 ]
 
 
