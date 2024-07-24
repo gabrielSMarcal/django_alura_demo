cd97d618d985b7659862a4030c6f6ec463e1a2fa
yhamerith <gabriel.santos.marcar1@gmail.com>
2024-07-23 21:21:45 -0300

    Update de imagens e filtros

--- setup/settings.py
+++ setup/settings.py
@@ -105,7 +105,6 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
@@ -115,7 +114,12 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
+# Media Files
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
