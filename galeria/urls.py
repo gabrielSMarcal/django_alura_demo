55f8bbba6ab1cb6d6e96bfc5211ebe9665b0561b
yhamerith <gabriel.santos.marcar1@gmail.com>
2024-07-24 21:07:17 -0300

    Adições finais do gerenciamento Admin

--- galeria/urls.py
+++ galeria/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('imagem/<int:foto_id>', views.imagem, name='imagem'),
+    path('buscar/', views.buscar, name='buscar'),
 ]
 
 
--- galeria/views.py
+++ galeria/views.py
@@ -10,3 +10,13 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'galeria/imagem.html', {'fotografia': fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('-data_foto').filter(publicada=True)
+    
+    if 'buscar' in request.GET:
+        nome_buscado = request.GET['buscar']
+        if nome_buscado:
+            fotografias = fotografias.filter(nome__icontains=nome_buscado)
+    
+    return render(request, 'galeria/buscar.html', {'cards': fotografias})
