3481de18c02364ad1cca08161dc2adbf2cd9bef0
yhamerith <gabriel.santos.marcar1@gmail.com>
2024-07-25 21:15:51 -0300

    Novo app para usuários

--- setup/settings.py
+++ setup/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'galeria.apps.GaleriaConfig',
+    'usuarios.apps.UsuariosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- setup/urls.py
+++ setup/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('galeria.urls')),
     path('admin/', admin.site.urls),
-    
+    path('', include('galeria.urls')),
+    path('', include('usuarios.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
