814bfb36808d9f53fea79c0cf622bba2942a0050
yhamerith <gabriel.santos.marcar1@gmail.com>
2024-07-27 14:50:47 -0300

    Lógica de login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('login', views.login, name='login'),
     path('cadastro', views.cadastro, name='cadastro'),
-    #path('logout', views.logout, name='logout'),
+    path('logout', views.logout, name='logout'),
 ]
